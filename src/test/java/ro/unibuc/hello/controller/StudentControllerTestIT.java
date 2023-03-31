6c2c6c80e0ca6cc03f8b0ecf5ca364db39fc6796
Alexandru NICOI <alexandru.nicoi@s.unibuc.ro>
2023-03-31 10:08:15 +0300

    CR Recomandations

--- src/test/java/ro/unibuc/hello/controller/StudentControllerTestIT.java
+++ src/test/java/ro/unibuc/hello/controller/StudentControllerTestIT.java
@@ -1,12 +1,15 @@
-package ro.unibuc.hello.service;
+package ro.unibuc.hello.controller;
 
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.classic.methods.HttpPost;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.classic.methods.HttpUriRequest;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.ContentType;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.io.entity.EntityUtils;
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.io.entity.StringEntity;
+import io.cucumber.messages.internal.com.google.gson.Gson;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 
@@ -22,6 +25,8 @@ import org.springframework.http.*;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.JsonNode;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import ro.unibuc.hello.dto.StudentDto;
+import ro.unibuc.hello.models.StudentEntity;
+import ro.unibuc.hello.repositories.StudentRepository;
 import ro.unibuc.hello.repositories.TeacherRepository;
 
 import java.time.LocalDate;
@@ -31,31 +36,25 @@ import java.time.LocalDate;
 public class StudentControllerTestIT {
 
     @Autowired
-    public TeacherRepository teacherRepository;
+    public StudentRepository studentRepository;
 
     @Autowired
     public ModelMapper modelMapper;
-
-
-    @Before
-    public void setup() {
-        teacherRepository.deleteAll();
-    }
 
     @Test
     public void testAddStudent() throws Exception {
         StudentDto studentDto = new StudentDto();
         studentDto.setFirstName("John");
         studentDto.setLastName("Popescu");
-        studentDto.setId("21");
         studentDto.setClassName("10A");
-        studentDto.setBirthDay(LocalDate.parse("2000-11-03"));
+        //studentDto.setBirthDay(LocalDate.parse("2000-11-03"));
 
-        String studentDtoJson = new ObjectMapper().writeValueAsString(studentDto);
+        JSONObject json = new JSONObject(new Gson().toJson(studentDto));
+        json.put("birthDay", "2000-11-03");
 
         HttpUriRequest request = new HttpPost(createURLWithPort("/student"));
         request.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        request.setEntity(new StringEntity(studentDtoJson));
+        request.setEntity(new StringEntity(json.toString(), ContentType.APPLICATION_JSON));
 
         try (CloseableHttpClient httpClient = HttpClientBuilder.create().build();
              CloseableHttpResponse response = httpClient.execute(request)) {
@@ -67,7 +66,12 @@ public class StudentControllerTestIT {
 
             JsonNode jsonNode = new ObjectMapper().readTree(responseBody);
             Assertions.assertTrue(jsonNode.get("success").asBoolean());
-            Assertions.assertEquals(studentDto.toString(), jsonNode.get("message").asText());
+            StudentEntity student = studentRepository.findByFirstNameAndLastName("John", "Popescu");
+            if (student != null) {
+                studentRepository.delete(student);
+            }
+            Assertions.assertNotNull(student);
+
         }
     }
 
